62a177d87c8c603ae153628f410a543243bc78aa
swlee9087 <sw_lee9087@naver.com>
2021-07-19 12:58:05 +0900

    first commit

---
+++ oop/calculator.py
@@ -0,0 +1,33 @@
+class Calculator(object):
+
+    def __init__(self, num1, num2):
+        self.num1 = num1
+        self.num2 = num2
+
+    def add(self):
+        return self.num1 + self.num2
+
+    def subtract(self):
+        return self.num1 - self.num2
+
+    def multiply(self):
+        return self.num1 * self.num2
+
+    def divide(self):
+        return self.num1 / self.num2
+
+    @staticmethod
+    def main():
+        while 1:
+            menu = input('0=Exit 1=Calculator\n')
+            if menu == '0':
+                break
+            elif menu == '1':
+                num1 = int(input('first number : '))
+                num2 = int(input('second number : '))
+                calc = Calculator(num1, num2)
+                print('*'*100)
+                print(f'{calc.num1}+{calc.num2} = {calc.add()}')
+                print(f'{calc.num1}-{calc.num2} = {calc.subtract()}')
+                print(f'{calc.num1}*{calc.num2} = {calc.multiply()}')
+                print(f'{calc.num1}/{calc.num2} = {calc.divide()}')
